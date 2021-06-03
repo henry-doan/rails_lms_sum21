6864859e1eaa1a29cdc906787a4ef31315f5a366
Henry Doan <henrydoan96@gmail.com>
2021-06-02 21:06:31 -0600

    end of lecture

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,8 +1,34 @@
 class EnrollmentsController < ApplicationController
+  before_action :set_course
+
   def index
+    # @enrollments = @course.enrollments
+    @teachers = @course.enrollments.where(role: 'teacher')
+    @tas = @course.enrollments.where(role: 'ta')
+    @students = @course.enrollments.where(role: 'student')
+    render component: "Enrollments", props: { 
+      course: @course, 
+      teachers: @teachers, 
+      tas: @tas, 
+      students: @students,
+      users: User.all 
+    }
   end
 
   def new
+    @users = User.all - @course.users 
+    @enrollment = @course.enrollments.new
+    render component: "EnrollmentNew", props: { course: @course, enrollment: @enrollment, users: @users }
+  end
+
+  def create 
+    @enrollment = @course.enrollments.new(enrollment_params)
+    @users = User.all - @course.users 
+    if @enrollment.save 
+      redirect_to course_enrollments_path(@course)
+    else
+      render component: "EnrollmentNew", props: { course: @course, enrollment: @enrollment, users: @users }
+    end
   end
 
   def show
@@ -10,4 +36,13 @@ class EnrollmentsController < ApplicationController
 
   def edit
   end
+
+  private 
+    def set_course
+      @course = Course.find(params[:course_id])
+    end
+
+    def enrollment_params
+      params.require(:enrollment).permit(:role, :user_id)
+    end
 end
---
+++ app/javascript/components/Enrollments.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Enrollments = ({ course, tas, teachers, students, users }) => {
+
+  const displayUser = (id) => {
+    let fullName 
+    users.map( (u) => {
+      if (u.id === id) {
+        fullName = u.first_name + " " + u.last_name
+      }
+    })
+    return fullName
+  }
+
+  return (
+    <>
+      <h1>{course.name}'s Enrollments</h1>
+      <a href={`/courses/${course.id}/enrollments/new`}>Add enrollment</a>
+      <br />
+      <h2>Teachers</h2>
+      { teachers.map( (t) => (
+        <div>
+          <p>{displayUser(t.user_id)}</p>
+          <a href={`/courses/${course.id}/enrollments/${t.id}`} data-method='delete'>Delete</a>
+        </div>
+      ))}
+      <h2>Tas</h2>
+      { tas.map( (ta) => (
+        <div>
+          <p>{displayUser(ta.user_id)}</p>
+          <a href={`/courses/${course.id}/enrollments/${ta.id}`} data-method='delete'>Delete</a>
+        </div>
+      ))}
+      <h2>Students</h2>
+      { students.map( (s) => (
+        <div>
+          <p>{displayUser(s.user_id)}</p>
+          <a href={`/courses/${course.id}/enrollments/${s.id}`} data-method='delete'>Delete</a>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default Enrollments;
